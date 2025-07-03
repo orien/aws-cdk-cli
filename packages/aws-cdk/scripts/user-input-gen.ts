64d88d9b8358aaa52e4250f94e78fc02631621cc
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2025-07-02 22:52:19 -0400

    chore(cli): cli type registry json (#682)
    Pulling out of #631.
    
    This is relevant for telemetry but also helpful in general to compile
    the [dynamic] source of truth in `cli-config.ts` into a static json for
    our viewing pleasure.
    
    ---
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache-2.0 license

--- packages/aws-cdk/scripts/user-input-gen.ts
+++ packages/aws-cdk/scripts/user-input-gen.ts
@@ -1,10 +1,11 @@
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { renderYargs, renderUserInputType, renderUserInputFuncs } from '@aws-cdk/user-input-gen';
 import { makeConfig, YARGS_HELPERS } from '../lib/cli/cli-config';
 
 async function main() {
   const config = await makeConfig();
+  fs.writeJSONSync('./lib/cli/cli-type-registry.json', config, { spaces: 2 });
   fs.writeFileSync('./lib/cli/parse-command-line-arguments.ts', await renderYargs(config, YARGS_HELPERS));
   fs.writeFileSync('./lib/cli/user-input.ts', await renderUserInputType(config));
   fs.writeFileSync('./lib/cli/convert-to-user-input.ts', await renderUserInputFuncs(config));
