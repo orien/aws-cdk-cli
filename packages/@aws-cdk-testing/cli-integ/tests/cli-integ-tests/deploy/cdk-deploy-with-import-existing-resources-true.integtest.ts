aea4df0681dec2439d7ddc2336e63a288855b161
Eli Polonsky <Eli.polonsky@gmail.com>
2026-03-27 04:02:53 -0400

    chore: restrict integ tests to their supported regions (#1267)
    Now that atmosphere supports asking for a specific region, we can apply
    the necessary region limitations per test, instead of globally in
    atmosphere itself.
    
    ### Checklist
    - [ ] This change contains a major version upgrade for a dependency and
    I confirm all breaking changes are addressed
      - Release notes for the new version:
    
    ---
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache-2.0 license
    
    ---------
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/deploy/cdk-deploy-with-import-existing-resources-true.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/deploy/cdk-deploy-with-import-existing-resources-true.integtest.ts
@@ -1,5 +1,12 @@
 import { DescribeStacksCommand, ListChangeSetsCommand } from '@aws-sdk/client-cloudformation';
 import { integTest, withDefaultFixture } from '../../../lib';
+import * as regions from '../../../lib/regions';
+
+const SUPPORTED_REGIONS = regions.allBut([
+  // quirk: ImportExistingResources behaves differently in these regions.
+  'eu-south-1',
+  'ap-southeast-3',
+]);
 
 integTest('deploy with import-existing-resources true', withDefaultFixture(async (fixture) => {
   const stackArn = await fixture.cdkDeploy('test-2', {
@@ -23,5 +30,5 @@ integTest('deploy with import-existing-resources true', withDefaultFixture(async
   expect(changeSets.length).toEqual(1);
   expect(changeSets[0].Status).toEqual('CREATE_COMPLETE');
   expect(changeSets[0].ImportExistingResources).toEqual(true);
-}));
+}, { aws: { regions: SUPPORTED_REGIONS } }));
 
--- packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/hotswap/cdk-hotswap-deployment-supports-appsync-apis-with-many-functions.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/hotswap/cdk-hotswap-deployment-supports-appsync-apis-with-many-functions.integtest.ts
@@ -1,5 +1,14 @@
 import { DescribeStacksCommand } from '@aws-sdk/client-cloudformation';
 import { integTest, withDefaultFixture } from '../../../lib';
+import * as regions from '../../../lib/regions';
+
+const SUPPORTED_REGIONS = regions.allBut([
+  // AppSync isn't supported in these regions
+  'ap-southeast-5',
+  'ap-southeast-7',
+  'ca-west-1',
+  'mx-central-1',
+]);
 
 integTest('hotswap deployment supports AppSync APIs with many functions',
   withDefaultFixture(async (fixture) => {
@@ -29,6 +38,6 @@ integTest('hotswap deployment supports AppSync APIs with many functions',
     for (const i of Array(50).keys()) {
       expect(deployOutput).toContain(`AWS::AppSync::FunctionConfiguration 'appsync_function${i}' hotswapped!`);
     }
-  }),
+  }, { aws: { regions: SUPPORTED_REGIONS } }),
 );
 
